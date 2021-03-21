9e672b3c125611869cb238e4cd8bbc4cecd017d8
GauravAB <bhokare.gaurav@gmail.com>
2021-03-21 07:47:12 +0530

    A hittable scene

--- main.cpp
+++ main.cpp
@@ -9,12 +9,24 @@
 
 #define M_PI 3.14159265
 
+struct Material
+{
+	Material(const Vec3f& color) : diffuse_color(color) {}
+	Material() : diffuse_color() {}
+
+	Vec3f diffuse_color;
+};
+
+
+
 struct Sphere
 {
 	Vec3f center;
 	float radius;
+	Material material;
 
 	Sphere(const Vec3f& c, const float& r) : center(c), radius(r) {}
+	Sphere(const Vec3f& c, const float& r, const Material& m) : center(c), radius(r),material(m) {}
 
 	bool ray_intersect(const Vec3f& orig, const Vec3f& dir, float& t0) const
 	{
@@ -33,20 +45,50 @@ struct Sphere
 	}
 };
 
-Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const Sphere& sphere)
+
+//scene hitter
+bool scene_intersect(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& spheres, Vec3f& hit,
+	Vec3f& N, Material& material)
 {
 	float sphere_dist = std::numeric_limits<float>::max();
-	if (!sphere.ray_intersect(orig, dir, sphere_dist))
+	
+	//for all the explicit geometries in the scene
+	for (size_t i = 0; i < spheres.size(); i++)
 	{
-		return Vec3f(0.2, 0.7, 0.8); //background color
+		float dist_i;
+		//if intersects then get the info
+		if ( spheres[i].ray_intersect(orig, dir, dist_i) && (dist_i < sphere_dist))
+		{
+			sphere_dist = dist_i;
+			hit = orig + dir * dist_i;
+			N = (hit - spheres[i].center).normalize();
+			material = spheres[i].material;
+		}
 	}
-	//sphere colour
-	return Vec3f(0.4, 0.4, 0.3);
+
+	//we have a sphere which is hit and within the distance of 1000 far
+	return sphere_dist < 1000;
+}
+
+
+Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere> &spheres)
+{
+	Vec3f point, N;
+	Material material;
+
+	//not a geometry then the background colour
+	if (!scene_intersect(orig, dir, spheres, point, N, material))
+	{
+		return Vec3f(0.2, 0.7, 0.8);
+	}
+
+	return material.diffuse_color;
+
 }
 
 
 
-void render( const Sphere& sphere)
+void render( const std::vector<Sphere> &spheres)
 {
 	const int width = 1024;
 	const int height = 768;
@@ -63,13 +105,13 @@ void render( const Sphere& sphere)
 			float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
 			Vec3f dir = Vec3f(x, y, -1).normalize();
 
-			framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
+			framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, spheres);
 		}
 	}
 
 	std::ofstream ofs; // save the framebuffer to file
 
-	ofs.open("out1.ppm", std::ios::binary);
+	ofs.open("out2.ppm", std::ios::binary);
 
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 
@@ -80,14 +122,23 @@ void render( const Sphere& sphere)
 			ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
 		}
 	}
-
-
 	ofs.close();
 }
+
+
 int main()
 {
-	Sphere sphere(Vec3f(-3, 0, -16), 2);
-	render(sphere);
+	Material ivory(Vec3f(0.4, 0.4, 0.3));
+	Material red_rubber(Vec3f(0.3, 0.1, 0.1));
+
+	std::vector<Sphere> spheres;
+	spheres.push_back(Sphere(Vec3f(-3,      0, -16), 2, ivory));
+	spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+	spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
+	spheres.push_back(Sphere(Vec3f(   7,    5, -18), 4, red_rubber));
+
+	render(spheres);
+
 	return 0;
 }
 
